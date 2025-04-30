6b8cde1df032ad22b3aa3d3dab3cb06c4d908fdf
aeliyadevs <aeliyadevs@gmail.com>
2025-04-30 09:18:48 +0545

    store task items in an array and update tasks on screen without reload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let todoItems = "";
+let todoItems = [];
 
 let inputBox = document.getElementById("input-box");
 inputBox.addEventListener("change", setTaskItem);
@@ -13,18 +13,26 @@ function setTaskItem(event) {
 let addButton = document.getElementById("add-button");
 addButton.addEventListener("click", saveTask);
 
+let count = 0;
+
 function saveTask(e) {
   if (task == "") {
     alert("Enter your task first");
   } else {
     taskArray.push(task);
-    localStorage.setItem("Tasks", taskArray);
+    // console.log(taskArray);
+    // console.log(JSON.stringify(taskArray));
+    localStorage.setItem("Tasks", JSON.stringify(taskArray));
+
+    // count++;
+    // localStorage.setItem("Task" + count, task);
   }
+  createItem();
 }
 
 function getItems() {
-  todoItems = localStorage.getItem("Tasks");
-  console.log(todoItems);
+  const items = localStorage.getItem("Tasks");
+  todoItems = JSON.parse(items);
 }
 getItems();
 
@@ -32,30 +40,40 @@ let itemsSpace = document.getElementById("task-items");
 
 // create todo list item
 function createItem() {
-  // create main item div
-  let item = document.createElement("div");
-  item.classList.add("item");
+  getItems();
+  itemsSpace.innerHTML = "";
+  console.log(todoItems);
+  if (todoItems != null) {
+    for (i = 0; i < todoItems.length; i++) {
+      // create main item div
+      let item = document.createElement("div");
+      item.classList.add("item");
+      // create child elements
+      let checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
 
-  // create child elements
-  let checkbox = document.createElement("input");
-  checkbox.type = "checkbox";
+      let itemText = document.createElement("p");
+      itemText.innerHTML = todoItems[i];
 
-  let itemText = document.createElement("p");
-  itemText.innerHTML = todoItems;
+      let progress = document.createElement("div");
+      progress.classList.add("progress");
 
-  let progress = document.createElement("div");
-  progress.classList.add("progress");
+      let icon = document.createElement("i");
+      icon.classList.add("fa-solid", "fa-xmark");
 
-  let icon = document.createElement("i");
-  icon.classList.add("fa-solid", "fa-xmark");
+      // append children to item parent
+      item.appendChild(checkbox);
+      item.appendChild(itemText);
+      item.appendChild(progress);
+      item.appendChild(icon);
 
-  // append children to item parent
-  item.appendChild(checkbox);
-  item.appendChild(itemText);
-  item.appendChild(progress);
-  item.appendChild(icon);
-
-  // append children to main parent
-  itemsSpace.appendChild(item);
+      // append children to main parent
+      itemsSpace.appendChild(item);
+    }
+  } else {
+    const message = document.createElement("p");
+    message.innerHTML = "Nothing here";
+    itemsSpace.appendChild(message);
+  }
 }
 createItem();
